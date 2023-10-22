cf643ac55f5419fa13673c39c2e9735c162e37e3
TuanTNT19 <tuantnt08@gmail.com>
2023-10-22 14:21:59 +0700

    fix in main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,33 +1,6 @@
 
 
-
 #include "stdio.h"
-<<<<<<< HEAD
-#include "Timer.h"
-#include "ADC.h"
-#include "GPIOP.h"
-#include "UART.h"
-uint16_t adc_value[2];
-uint8_t channel[2]={1,0};
-char str[10];
-
-
-int main()
-{
-	  USART2_Init();
-		TIM2_config();
-		ADC1_Config_Multi(2,channel);
-		DMA_Init();
-  	ADC1_Read_DMA((uint32_t)&ADC1->DR, (uint32_t)adc_value, 2);
-	  USART2_Config(9600);
-	while(1)
-	{
-	int len1 = sprintf(str,"%4d %4d",adc_value[0],adc_value[1]);
-	USART2_Send_String((char *)(str));
-	delay_ms(100);	
-	}
-	
-=======
 #include "main.h"
 #include "cmsis_os.h"
 #include "ADC.h"
@@ -106,5 +79,4 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE BEGIN Callback 1 */
 
   /* USER CODE END Callback 1 */
->>>>>>> Develop
 }
